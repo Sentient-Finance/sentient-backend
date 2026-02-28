d74975a8bcfae5ac86f2965afb42710d616c3a55
Vanthang <vanthang23032001@gmail.com>
2026-02-28 23:09:39 +0700

    fix(alembic): correct import order in vault read tables migration script

--- alembic/versions/20260228_0001_create_vault_read_tables.py
+++ alembic/versions/20260228_0001_create_vault_read_tables.py
@@ -5,9 +5,9 @@ Revises:
 Create Date: 2026-02-28 08:40:00
 """
 
-from alembic import op
 import sqlalchemy as sa
 
+from alembic import op
 
 # revision identifiers, used by Alembic.
 revision = "20260228_0001"
