a3b77adfbc1c898b44cdf2a47b52a049df305a43
le-dat <lqdat87@gmail.com>
2026-03-03 16:50:26 +0700

    Remove explicit `db.commit()` from the `get_db` session generator.

--- libs/db/session.py
+++ libs/db/session.py
@@ -36,7 +36,6 @@ def get_db() -> Generator[Session, None, None]:
     db = SessionLocal()
     try:
         yield db
-        db.commit()
     except Exception:
         db.rollback()
         raise
